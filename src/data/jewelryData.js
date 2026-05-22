d5afb18a4b08c333d5278ef46abedeb21976d129
stanislavapolozova <stanislavapolozova@gmail.com>
2026-05-22 09:34:05 +0300

    Добавлен массив jewelryData.js с украшениями

---
+++ src/data/jewelryData.js
@@ -0,0 +1,81 @@
+export const jewelryData = [
+  {
+    id: 1,
+    name: "Ожерелье «Россыпь камней»",
+    category: "necklaces",
+    shortDesc: "Розовый кварц, лунный камень, аквамарин, горный хрусталь",
+    fullDesc: "Нежное ожерелье ручной работы. Камень приносит спокойствие и ясность мыслей.",
+    price: "1 200 ₽",
+    image: "src/assets/necklace2.jpg"
+  },
+
+  {
+    id: 2,
+    name: "Ожерелье «Роскошь»",
+    category: "necklaces",
+    shortDesc: "Натуральные коралл, долматиновая яшма и жемчуг",
+    fullDesc: "Элегантное украшение к вечернему образу.",
+    price: "2 900 ₽",
+    image: "src/assets/necklace3.jpg"
+  },
+
+  {
+    id: 3,
+    name: "Ожерелье «Морская звезда»",
+    category: "necklaces",
+    shortDesc: "Натуральные ракушка и жемчуг",
+    fullDesc: "Гармоничное и нежное дополнение к летним образам.",
+    price: "2 600 ₽",
+    image: "src/assets/necklace1.jpg"
+  },
+
+  {
+    id: 4,
+    name: "Браслет «Гармония»",
+    category: "bracelets",
+    shortDesc: "Натуральные горный хрусталь и гематит",
+    fullDesc: "Минималистичное дополение к любому образу. Регулируется под любой размер.",
+    price: "1 100 ₽",
+    image: "src/assets/bracelet1.jpg"
+  },
+
+  {
+    id: 5,
+    name: "Браслет «Луч»",
+    category: "bracelets",
+    shortDesc: "Натуральные горный хрусталь и оникс",
+    fullDesc: "Изящный браслет с ярким акцентом. Регулируется под любой размер.",
+    price: "1 100 ₽",
+    image: "src/assets/bracelet2.jpg"
+  },
+
+  {
+    id: 6,
+    name: "Браслет «Звёздочка»",
+    category: "bracelets",
+    shortDesc: "Натуральные горный хрусталь и лунный камень",
+    fullDesc: "Нежное сочетание блеска и прозрачности камней. Регулируется под любой размер.",
+    price: "1 100 ₽",
+    image: "src/assets/bracelet3.jpg"
+  },
+
+  {
+    id: 7,
+    name: "Серьги «Морская звезда»",
+    category: "earrings",
+    shortDesc: "Натуральные ракушка и жемчуг",
+    fullDesc: "Идеальное дополнеие к ожерелью.",
+    price: "1 600 ₽",
+    image: "src/assets/earrings1.jpg"
+  },
+
+  {
+    id: 8,
+    name: "Серьги «Роскошные»",
+    category: "earrings",
+    shortDesc: "Натуральные коралл, долматиновая яшма и жемчуг",
+    fullDesc: "Гармоничное дополнение к ожерелью.",
+    price: "1 600 ₽",
+    image: "src/assets/earrings2.jpg"
+  }
+];
