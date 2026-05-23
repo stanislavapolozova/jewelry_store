701c975bd0ea9427ba8d57495d23ad3ff08e0c8d
Monarch_Inc <danymaybelol@gmail.com>
2026-05-23 14:18:45 +0300

    Update Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+// подвал сайта
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container footer__container">
+        <p>
+          &copy; 
+          2026 KISÉTI — украшения ручной работы
+        </p>
+        <div className="footer__social">
+          <a href="https://t.me/kiseti_jw" target="_blank">
+            Telegram
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
