cd4f210aeb81e03ee55c24cdce97c106b9561873
stanislavapolozova <stanislavapolozova@gmail.com>
2026-05-29 10:32:25 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import PhoneMenu from '../PhoneMenu/PhoneMenu';
 import './Header.css';
 
 function Header() {
-    // состояние для открытия/закрытия мобильного меню
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
     return (
         <header className="header">
             <div className="container header__container">
-                <Link to="/" className="header__logo">
-                    KISÉTI
-                </Link>
-                <button className="header__burger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-                <nav className={`header__nav ${isMenuOpen ? 'header__nav--open' : ''}`}>
-                    <NavLink to="/" onClick={() => setIsMenuOpen(false)}>
-                        Главная
-                    </NavLink>
-                    <NavLink to="/catalog" onClick={() => setIsMenuOpen(false)}>
-                        Каталог
-                    </NavLink>
-                    <NavLink to="/about" onClick={() => setIsMenuOpen(false)}>
-                        О магазине
-                    </NavLink>
-                    <NavLink to="/contacts" onClick={() => setIsMenuOpen(false)}>
-                        Контакты
-                    </NavLink>
+                <Link to="/" className="header__logo">KISÉTI</Link>
+                <PhoneMenu />
+                <nav className="header__nav">
+                    <NavLink to="/">Главная</NavLink>
+                    <NavLink to="/catalog">Каталог</NavLink>
+                    <NavLink to="/about">О магазине</NavLink>
+                    <NavLink to="/contacts">Контакты</NavLink>
                 </nav>
             </div>
         </header>
